d605bc4a15132adc07f0055c7815753e555bde86
lhang662543608 <ljh_662543608@163.com>
2019-12-29 11:07:05 +0800

    Spring Initializr————使用快速向导创建SpringBoot应用

---
+++ SpringBoot/spring-boot-01-helloworld-quick/src/main/java/sylu/springboot/controller/HelloController.java
@@ -0,0 +1,16 @@
+package sylu.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//这个类的所有方法返回的数据直接写给浏览器。（如果是对象还可以转为json数据）
+/*@ResponseBody
+@Controller*/
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world quick!";
+    }
+}
---
+++ SpringBoot/spring-boot-01-helloworld-quick/src/test/java/sylu/springboot/SpringBoot01HelloworldQuickApplicationTests.java
@@ -0,0 +1,14 @@
+package sylu.springboot;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot01HelloworldQuickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
