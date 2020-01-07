86d83946b79af8a3877f4b5117a1f3793480a1d3
lhang <ljh_662543608@163.com>
2020-01-07 18:30:22 +0800

    springboot_AMQP消息队列

---
+++ SpringBoot/springboot-08-amqp/src/main/java/sylu/springboot08amqp/service/BookService.java
@@ -0,0 +1,21 @@
+package sylu.springboot08amqp.service;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import sylu.springboot08amqp.been.Book;
+
+@Service
+public class BookService {
+
+    @RabbitListener(queues = "atguigu.news")
+    public void receiveMeg(Book book){
+        System.out.println("收到消息:"+book);
+    }
+
+    @RabbitListener(queues = "atguigu")
+    public void receiveMeg(Message message){
+        System.out.println(message.getBody());
+        System.out.println(message.getMessageProperties());
+    }
+}
