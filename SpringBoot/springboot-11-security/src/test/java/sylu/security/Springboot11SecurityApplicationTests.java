3d0adeb136b36820ed0ccb149e191d806267f954
lhang <ljh_662543608@163.com>
2020-01-10 12:57:33 +0800

    springboot-security

---
+++ SpringBoot/springboot-11-security/src/test/java/sylu/security/Springboot11SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package sylu.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot11SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
